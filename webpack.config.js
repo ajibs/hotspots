7649d2f3613d26ce658439cc0a73dc28545e9989
bolu ajibs <bolu.ajibawo@gmail.com>
2017-11-01 14:50:12 +0100

    refactored starter files

--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,12 @@ const config = {
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true
     }),
+    /**
+     * Many libraries will key off the process.env.NODE_ENV variable to determine what should be included in the library.
+     * For example, when not in production some libraries may add additional logging and testing to make debugging easier
+     * with process.env.NODE_ENV === 'production' they might drop or add significant portions of code to optimize how things run for your actual users
+     * We use webpack's built in DefinePlugin to define this variable for all our dependencies
+     */
     new webpack.DefinePlugin({
       // plugin does a direct text replacement; hence the double quotes
       'process.env.NODE_env': '"production"'
